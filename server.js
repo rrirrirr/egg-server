581b04fa35a93b77149284de13863c39605d7e14
rrirrirr <drpihl@gmail.com>
2023-04-08 00:22:46 +0200

    add will egg with pattern when starting server

--- server.js
+++ server.js
@@ -18,16 +18,72 @@ const createDefaultGrid = (width, height) => {
     .map(() => Array(width).fill(defaultColor));
 };
 
-const storedColors = createDefaultGrid(20, 20);
+const createRandomColoredEgg = (width, height) => {
+  const colors = [
+    "#FFB6C1",
+    "#FFDAB9",
+    "#E6E6FA",
+    "#B0E0E6",
+    "#F0E68C",
+    "#FFC0CB",
+    "#FFE4E1",
+    "#D8BFD8",
+    "#FFDEAD",
+    "#C6E2FF",
+    "#BFEFFF",
+    "#F0FFF0",
+  ];
+
+  const centerX = Math.floor(width / 2);
+  const centerY = Math.floor(height / 2);
+  const outlineThickness = 1;
+
+  const grid = Array(height)
+    .fill(null)
+    .map(() => Array(width).fill("transparent"));
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const dx = x - centerX;
+      const dy = y - centerY;
+      const distanceSquared = dx * dx + dy * dy;
+      const outerRadius = (width * height) / 4;
+      const innerRadius = outerRadius - outlineThickness;
+      if (distanceSquared <= outerRadius && distanceSquared >= innerRadius) {
+        // Diagonal stripes pattern
+        const colorIndex = Math.floor((x + y) / 4) % colors.length;
+        grid[y][x] = colors[colorIndex];
+      }
+    }
+  }
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const dx = x - centerX;
+      const dy = y - centerY;
+      const distanceSquared = dx * dx + dy * dy;
+      const outerRadius = (width * height) / 4;
+      const innerRadius = outerRadius - outlineThickness;
+
+      // Check if the position is within the egg shape
+      if (distanceSquared <= outerRadius) {
+        // Diagonal stripes pattern
+        const colorIndex = Math.floor((x + y) / 4) % colors.length;
+        grid[y][x] = colors[colorIndex];
+      }
+    }
+  }
+  return grid;
+};
+
+const storedColors = createRandomColoredEgg(20, 20);
 
 io.on("connection", (socket) => {
   console.log("New client connected");
 
   socket.on("paint", (data) => {
-    console.log("painted");
     storedColors[data.y] = storedColors[data.y] || [];
     storedColors[data.y][data.x] = data.color;
-
     // Broadcast the paint event to all other clients
     socket.broadcast.emit("paint", data);
   });
